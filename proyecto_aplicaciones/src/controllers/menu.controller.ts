c6bc305c9101d68845fda4de9d9db4c266837684
Idaly García <garciaidaly386@gmail.com>
2025-07-21 02:13:33 -0600

    ...

--- proyecto_aplicaciones/src/controllers/menu.controller.ts
+++ proyecto_aplicaciones/src/controllers/menu.controller.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
-import Menu from "../models/menu"; // ✅ Importación corregida
-import User from "../models/user";
-import Role from "../models/role";
+import Menu from "../models/menu";
+import { User } from "../models/user";
+import { Role } from "../models/role";
 
 export const getMenuByRole = async (
-  req: Request<any, any, { userId: string }>, // ✅ Tipo corregido para body.userId
+  req: Request<any, any, { userId: string }>,
   res: Response
 ) => {
   try {
-    const userId = req.body.userId || (req as any).userId; // Backup si viene desde middleware u otro campo
+    const userId = req.body.userId || (req as any).userId;
     const user = await User.findById(userId).populate("role");
 
     if (!user) {
       return res.status(404).json({ message: "Usuario no encontrado" });
     }
 
-    const roleId = (user.role as any)._id; // ✅ Tipado flexible para evitar error de compilación
+    const roleId = (user.role as any)._id;
     const menuOptions = await Menu.find({ roles: roleId });
 
     res.status(200).json(menuOptions);
