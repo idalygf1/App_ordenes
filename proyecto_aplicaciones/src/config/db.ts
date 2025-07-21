d7a4fe1e789449fcd972c66e7b9d7553263d86a5
Idaly García <garciaidaly386@gmail.com>
2025-07-20 20:32:39 -0600

    ..

--- proyecto_aplicaciones/src/config/db.ts
+++ proyecto_aplicaciones/src/config/db.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const connectDBMongo = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/mi_basededatos");
-    console.log(" Conectado a MongoDB");
+    const mongoURI = process.env.MONGO_URI as string;
+    await mongoose.connect(process.env.MONGO_URI || "");
+    console.log("✅ Conectado a MongoDB");
   } catch (error) {
-    console.error(" Error al conectar a MongoDB:", error);
+    console.error("❌ Error al conectar a MongoDB:", error);
     throw error;
   }
 };
