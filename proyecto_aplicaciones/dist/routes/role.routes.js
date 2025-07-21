18f6f2d8b30cd049a68225b1c4d29bea3f9036e2
Idaly García <garciaidaly386@gmail.com>
2025-07-21 00:44:49 -0600

    fix: eliminar dist del repositorio y limpiar build

--- proyecto_aplicaciones/dist/routes/role.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const role_controller_1 = require("../controllers/role.controller");
-const router = (0, express_1.Router)();
-router.post("/", role_controller_1.createRole); // ✅ Ya es función válida
-router.get("/", role_controller_1.getAllRoles);
-router.get("/menu/:roleName", role_controller_1.getMenuByRole); // ✅ Menú por rol
-exports.default = router;
--- proyecto_aplicaciones/dist/src/routes/menu.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const menu_controller_1 = require("../controllers/menu.controller");
-const authJwt_1 = require("../middleware/authJwt");
-const router = (0, express_1.Router)();
-router.get("/options", authJwt_1.verifyToken, menu_controller_1.getMenuByRole);
-exports.default = router;
